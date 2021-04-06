813f159229386a8cb5c1d4a5be21e507c6a9618e
Mr.Khawaja <alveehawak360@gmail.com>
2021-04-06 15:22:17 +0600

    auto add table

--- src/xyz/alveehawak/prime/Prime.java
+++ src/xyz/alveehawak/prime/Prime.java
@@ -4,6 +4,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.alveehawak.prime.mysql.basic.Config;
+import xyz.alveehawak.prime.mysql.database.MySQL;
+import xyz.alveehawak.prime.mysql.database.SQL;
 
 public class Prime extends JavaPlugin {
     public static Plugin plugin;
@@ -14,10 +17,13 @@ public class Prime extends JavaPlugin {
     @Override
     public void onEnable() {
         plugin = this;
-        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN+"EEEEEE Pixeledge er plugin on hoise");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN+"Plugin Turned On");
         xyz.alveehawak.prime.mysql.basic.Config.create();
         xyz.alveehawak.prime.mysql.database.MySQL.connect();
         if(xyz.alveehawak.prime.mysql.database.MySQL.isConnected()){
+            if(!SQL.tableExists(Config.getTable())){
+                MySQL.update("CREATE TABLE `"+Config.getDatabase()+"`.`"+ Config.getTable()+"` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , UNIQUE `ID` (`id`)) ENGINE = InnoDB;");
+            }
             Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA+"Database Initiated");
         }
         this.getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
@@ -25,16 +31,7 @@ public class Prime extends JavaPlugin {
 
     @Override
     public void onDisable(){
-        Bukkit.getConsoleSender().sendMessage(ChatColor.RED+"Off korlo ke?");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.RED+"Plugin turned off");
         xyz.alveehawak.prime.mysql.database.MySQL.disconnect();
-    }
-
-    private boolean validDouble(String s) {
-        try {
-            Double.valueOf(s);
-            return true;
-        } catch (Exception var3) {
-            return false;
-        }
     }
 }
